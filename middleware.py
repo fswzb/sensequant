ee61c4988263b1b0f6cd51cf9141044b8e9184a3
rylanchiu <rylanlzc@gmail.com>
2016-07-15 20:28:07 +0800

    support multi stock

--- middleware.py
+++ middleware.py
@@ -1,2 +1,8 @@
 import numpy as np 
 import pandas as pd 
+
+if __name__ == '__main__':
+    df_test = pd.read_csv('cache.txt')
+    df_test = df_test.drop('index', axis=1)
+    WHOLE_PERIOD = df_test.data.unique()
+    for time in WHOLE_PERIOD:
